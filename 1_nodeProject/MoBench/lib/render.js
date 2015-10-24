cbbad7714e2c5a259e57034a977b6e8c439ed533
zhuzhen.bk <zhuzhen.bk@alibaba-inc.com>
2015-10-24 20:01:35 +0800

    nodejs : 项目模板

---
+++ 1_nodeProject/MoBench/lib/render.js
@@ -0,0 +1,10 @@
+/**
+ * Created by bian17888 on 15/10/20.
+ */
+var views = require('co-views');
+
+// setup views mapping .html
+// to the swig template engine
+module.exports = views(__dirname + './../views', {
+	map: { html: 'swig' }
+});
